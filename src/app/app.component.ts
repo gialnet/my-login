5d8147c0e143add59c4c4211c658b984746a07ab
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2018-01-23 21:47:52 +0000

    login form v3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  enjson = JSON.parse(JSON.stringify(this.heroes));
+  // UserName: string, PassWord: string
+  SummitJSON(UserName: string, PassWord: string) {
+    var unarray: string [] = [];
+    unarray.push('username: '+UserName);
+    unarray.push('password: '+PassWord);
+    this.enjson=JSON.parse(JSON.stringify(unarray));
+    console.log(this.enjson);
+    console.log(UserName);
+    //console.log("Hola de nuevo");
+    //this.title="Hola otra vez";
+  }
 }
